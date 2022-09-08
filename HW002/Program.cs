6c3e0d1941bce2e26e9b7f931a42d7ae3971caf9
Valentin <dirk_31@mail.ru>
2022-09-08 21:31:14 +0700

    Hw2

---
+++ HW002/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введи число: ");
+int anyNumber = Convert.ToInt32(Console.ReadLine());
+string anyNumberText = Convert.ToString(anyNumber);
+
+
+if (anyNumberText.Length > 2){
+  Console.WriteLine("третья цифра -> " + anyNumberText[2]);
+}
+
+else {
+  Console.WriteLine("-> третьей цифры нет");
+}
