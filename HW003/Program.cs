f3d9edba8f1332f5704ba0ab794f62ec8c7f03af
Valentin <dirk_31@mail.ru>
2022-09-08 21:35:43 +0700

    Hw3

---
+++ HW003/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введи цифру, обозначающую день недели: ");
+int dayNumber = Convert.ToInt32(Console.ReadLine());
+
+void CheckingTheDayOfTheWeek (int dayNumber) {
+ 
+
+  if (dayNumber == 6 || dayNumber == 7) {
+  Console.WriteLine("(выходной) -> да");
+  }
+  else if (dayNumber < 1 || dayNumber > 7) {
+    Console.WriteLine("не день недели");
+  }
+  else Console.WriteLine("(не выходной) -> нет");
+}
+
+
+CheckingTheDayOfTheWeek(dayNumber);
