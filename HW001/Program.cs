90a4809a1c895f2d39a26c6192a8cba357f9ade3
Valentin <dirk_31@mail.ru>
2022-09-08 21:25:07 +0700

    HomeWork1

---
+++ HW001/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трёхзначное число ");
+
+    while(true)
+    {
+        String input = Console.ReadLine();
+        if(!input.Equals("exit"))
+            System.Console.WriteLine("{0}->{1}", input, input[1]);
+            else
+            break;
+    }
+
